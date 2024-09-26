ef6a877fa44a5660238d40f520ca5d09efea3736
Vanio <almeida.vanio@gmail.com>
2024-09-26 14:49:48 -0300

    feat: change gpu

--- setup.sh
+++ setup.sh
@@ -52,8 +52,18 @@ chmod +x gpu
 chmod +x cpu
 chmod +x gsbs
 
-./gpu --name=67 --size=2500000000000 --address=RTX-4090-default-gpu > output-gpu.txt 2>&1 &
+./gpu --name=67 --size=10000000000 --address=RTX-4090-default-gpu-1 > output-gpu-1.txt 2>&1 &
+./gpu --name=67 --size=20000000000 --address=RTX-4090-default-gpu-2 > output-gpu-2.txt 2>&1 &
+./gpu --name=67 --size=30000000000 --address=RTX-4090-default-gpu-3 > output-gpu-3.txt 2>&1 &
+./gpu --name=67 --size=40000000000 --address=RTX-4090-default-gpu-4 > output-gpu-4.txt 2>&1 &
+./gpu --name=67 --size=50000000000 --address=RTX-4090-default-gpu-5 > output-gpu-5.txt 2>&1 &
+./gpu --name=67 --size=60000000000 --address=RTX-4090-default-gpu-6 > output-gpu-6.txt 2>&1 &
+./gpu --name=67 --size=70000000000 --address=RTX-4090-default-gpu-7 > output-gpu-7.txt 2>&1 &
+./gpu --name=67 --size=20000000000 --address=RTX-4090-default-gpu-8 > output-gpu-8.txt 2>&1 &
+./gpu --name=67 --size=50000000000 --address=RTX-4090-default-gpu-9 > output-gpu-9.txt 2>&1 &
+
 ./cpu --name=67 --size=1000000000 --address=RTX-4090-default-cpu > output-cpu.txt 2>&1 &
+
 ./gsbs --name=135 --size=10000000000000000 -address=RTX-4090-default-gsbs > output-gsbs.txt 2>&1 &
 
 echo "All commands running in parallel!"
